040876341a6ae27f8be2762faf65e70c869c6309
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-05-14 20:46:22 +0100

    refactored

---
+++ todo_api/src/main.rs
@@ -0,0 +1,9 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::{serde::json::Json, State};
+
+use std::{io::ErrorKind, sync::Arc};
+use surrealdb::{sql::Object, Datastore, Session };
+
+use crate::db::{AffectedRows, DB};
